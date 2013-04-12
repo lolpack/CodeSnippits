c55b1a3335d5b6708b3752f16598d179b0e4b461
Aaron Pollack <pollackaaron@gmail.com>
2013-04-11 21:38:55 -0700

    Tic Tac Toe Game
    Console tic tac toe game to practice creating classes/objects in python

---
+++ Classes tic tac.py
@@ -0,0 +1,110 @@
+#By Aaron Pollack
+#Written in Python 3.3.0, adapted for Python 2.7.3
+#Tic Tac Toe Part 2
+
+class Board(object):
+    """Represents tic tac toe board"""
+    def __init__(self):
+        self.free_Move = ' '
+        self.Player_X = 'x'
+        self.Player_O = 'o'
+        self.moves = [self.free_Move]*9
+
+    _winning_rows = [[0,1,2],[3,4,5],[6,7,8], # up and down
+                        [0,3,6],[1,4,7],[2,5,8], # across
+                        [0,4,8],[2,4,6]]         # diagonal
+
+    def winner(self):
+        """Wining combinations. Returns Player or None"""
+        for row in Board._winning_rows:
+            if self.moves[row[0]] != self.free_Move and self.allEqual([self.moves[i] for i in row]):
+                return self.moves[row[0]]
+
+    def allEqual(self, list):
+        """returns True if all the elements in a list are equal, or if the list is empty."""
+        return not list or list == [list[0]] * len(list)
+
+
+
+    def getValidMoves(self):
+        """Returns list of valid moves"""
+        return [pos for pos in range(9) if self.moves[pos] == self.free_Move]
+
+    def gameOver(self):
+        return bool(self.winner()) or not self.getValidMoves()
+    def clear(self):
+        """Clears board"""
+        self.moves = [self.free_Move]*9
+
+class Player(object):
+
+    """ Represents player and his/her moves"""
+    def __init__(self, piece, moves):
+        self.moves = moves
+        self.piece = piece
+
+    def getMove(self):
+        print("Move Player %s" % self.piece)
+        move = int(input())
+        if -1 < move < 9:
+            for i in self.moves[move]:
+                if i == ' ':
+                    self.makeMove(move)
+                    print(self.moves)
+                    return True
+                else:
+                    print("That's not a valid move")
+        else:
+            print("That's not a valid move")
+
+    def makeMove(self, move):
+        self.moves[move] = self.piece
+
+    def __str__(self):
+        return "Player " + self.piece
+
+class Game(object):
+    """ Represents the tic tac toe game itself. Use this to initialize the game"""
+    _welcome_mes = """Welcome to Tic Tac Toe!
+
+The Game board is as follows:
+
+    _0_|_1_|_2_
+    _3_|_4_|_5_
+     6 | 7 | 8   """
+
+    def __init__(self):
+        self.board = Board()
+        self.playerX = Player('x', self.board.moves)
+        self.playerO = Player('o', self.board.moves)
+        self._currentTurn = self.playerX
+
+    def _swapCurrentTurn(self):
+        if self._currentTurn == self.playerX:
+            self._currentTurn = self.playerO
+        else: self._currentTurn = self.playerX
+
+    def start(self):
+        print(Game._welcome_mes)
+        while not self.isGameOver():
+            print("Turn: " + str(self._currentTurn))
+            if self._currentTurn.getMove():
+                self._swapCurrentTurn()
+        self._atGameEnd()
+
+    def _atGameEnd(self):
+        print(self.board.winner() + " has won the game")
+        print("Would you like to play again?")
+        answer = input()
+        if str(answer).lower() in ["yes", "y", "yeah"]:
+            self._restart()
+
+    def _restart(self):
+        self.board.clear()
+        self.playerX = Player('x', self.board.moves)
+        self.playerO = Player('o', self.board.moves)
+        self._currentTurn = self.playerX
+        self.start()
+
+    def isGameOver(self):                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+        return self.board.gameOver()
