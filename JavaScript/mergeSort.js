b5ec8f4a15cf5928ecd17b117ec8592845b2375f
Aaron Pollack <pollackaaron@gmail.com>
2013-12-20 08:57:36 -0800

    Update mergeSort.js
    Added comments

--- JavaScript/mergeSort.js
+++ JavaScript/mergeSort.js
@@ -3,19 +3,19 @@
 before it's returned*/
 
 function mergeSort(array) {
-	if (array.length < 2) {
+	if (array.length < 2) { //Base case to return array if 1 or 0
 		return array
 	}
-	var mid = Math.floor(array.length/2),
+	var mid = Math.floor(array.length/2), //Finds mid point in array
 		left = array.slice(0, mid),
 		right = array.slice(mid);
 
 
-	var leftSorted = mergeSort(left),
+	var leftSorted = mergeSort(left), //Recurse left half of array and right half
 		rightSorted = mergeSort(right);
 
 	var sorted = [];
-	while (leftSorted.length > 0 || rightSorted.length > 0) {
+	while (leftSorted.length > 0 || rightSorted.length > 0) { //If right or left sorted array is empty add the opposite array
 		if (leftSorted.length < 1) {
 			sorted = sorted.concat(rightSorted);
 			break;
@@ -25,6 +25,7 @@ function mergeSort(array) {
 			break;
 		}
 	var elem = (leftSorted[0] < rightSorted[0] ? leftSorted.shift() : rightSorted.shift())
+	//Sortinng sub routine that takes smaller front item from either right or left arrray and adds it to sorted.
 	sorted.push(elem);
 }
 	return sorted
