5f3845e2ed723f28f04aa91ed4e510ee07452cf9
Aaron Pollack <pollackaaron@gmail.com>
2013-12-19 21:58:17 -0800

    Update Stack.js
    Added more comments

--- JavaScript/Stack.js
+++ JavaScript/Stack.js
@@ -16,10 +16,10 @@ Stack.prototype.push = function (data) {
 };
 
 Stack.prototype.pop = function (callback) {
-	if (this.head === null || !this.head.hasOwnProperty('prev')) { //If head is empty 'underflow'
+	if (this.head === null || !this.head.hasOwnProperty('prev')) { //If head is empty or if head doesn't have a prev return 'underflow' 
 		return callback("Underflow", null);
 	} else {
-		var popped = this.head.data;
+		var popped = this.head.data; //If not empty, set the head to be the prev value and return data from the current head
 		this.head = this.head.prev;
 		return callback(null, popped);
 	}
@@ -32,12 +32,12 @@ var st = new Stack();
 st.push(9);
 st.push(8);
 st.push(78);
-st.pop(console.log);
-st.pop(console.log);
+st.pop(console.log); //should return 78
+st.pop(console.log); //should return 8
 console.log(st);
 
 var st2 = new Stack();
-st2.pop(console.log);
+st2.pop(console.log); // Underflow error
 
 var st3 = new Stack();
 st.push("adsfasdf");
@@ -55,4 +55,5 @@ st.push(45);
 st.push(45);
 st.push(45);
 st.push(45);
-console.log(st3);
+console.log(st3); //Empty stack
+console.log(st); //Large stack
