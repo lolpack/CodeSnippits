1cd6c5da8be9a2da92329fe80333e8af8be0db3e
Aaron Pollack <pollackaaron@gmail.com>
2013-12-19 23:40:29 -0800

    Update linkedList.js
    Added comments

--- JavaScript/linkedList.js
+++ JavaScript/linkedList.js
@@ -3,16 +3,16 @@
 
 
 var LinkedList = function () {
-	this.head = null;
+	this.head = null; //Simply singly linked list
 };
 
 LinkedList.prototype.insert = function(value) {
 	if (this.head === null) {
-		this.head = {data: value, next: null};
+		this.head = {data: value, next: null}; //If empy populated head with object literal
 	} else {
 		var temp = this.head;
 		while(temp.next !== null) {
-			temp = temp.next;
+			temp = temp.next; //Traverse current nodes to find first null value
 
 		}
 		temp.next = {data: value, next: null};
@@ -21,14 +21,29 @@ LinkedList.prototype.insert = function(value) {
 
 LinkedList.prototype.del = function (index) {
 	if (this.head === null) {
-		return console.log('No items to delete');
-	} else {
-		var temp = this.head;
-		while (temp.next.next !== null) {
-			console.log(temp)
-			temp = temp.next;
-		}
-		temp.next = temp.next.next;
+		return console.log('No items to delete'); //Check to see if there's data in the list
+		
+	else if (index === 0) {
+                this.head.data = this.head.next.data; //If first index then set head to next to next.next, skippinging middel value
+                this.head.next = this.head.next.next;
+        }  else {
+		var count = 0; //Keep track of how many times you traverse through the linked list
+                var temp = this.head;
+                while (count <= index) {
+                        if (count === index) {
+                                temp.data = temp.next.data;
+                                temp.next = temp.next.next;
+                                count++;
+                        }
+                        else {
+                                if (count+1 === index) {
+                                        temp.next = null;
+                                        break;
+                                }
+                                temp = temp.next;
+                                count++;
+
+                        }
 	}
 };
 
