7cc4c37e7f8a974a70edefeaf39e1b677310d4b6
Aaron Pollack <pollackaaron@gmail.com>
2014-01-01 18:55:51 -0800

    Adjusted spacing on merge sort

--- JavaScript/mergeSort.js
+++ JavaScript/mergeSort.js
@@ -6,7 +6,7 @@ function mergeSort(array) {
 	if (array.length < 2) {
 		return array
 	}
-	var mid = Math.floor(array.length/2),
+	var mid = Math.floor(array.length / 2),
 		left = array.slice(0, mid),
 		right = array.slice(mid);
 
@@ -24,12 +24,12 @@ function mergeSort(array) {
 			sorted = sorted.concat(leftSorted);
 			break;
 		}
-	var elem = (leftSorted[0] < rightSorted[0] ? leftSorted.shift() : rightSorted.shift())
-	sorted.push(elem);
-}
+		var elem = (leftSorted[0] < rightSorted[0] ? leftSorted.shift() : rightSorted.shift())
+		sorted.push(elem);
+	}
 	return sorted
 }
 
 //////TEST////////
 
-console.log(mergeSort([1,3,532,43,1,35,6,72,3,4,5,7,8]))
+console.log(mergeSort([1, 3, 532, 43, 1, 35, 6, 72, 3, 4, 5, 7, 8]))
